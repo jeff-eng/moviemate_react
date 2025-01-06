ba57c2fe048b0bbc8ef002566f83e88a79128bcd
jeff-eng <me@jeffeng.com>
2025-01-05 16:23:49 -0800

    Changed Links to NavLink to enable active styles for the tab bar links. CSS styling targeting classes for the active nav links.

--- src/components/tabbar/TabBar.jsx
+++ src/components/tabbar/TabBar.jsx
@@ -1,21 +1,45 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHouse,
+  faHeart,
+  faUserGear,
+} from '@fortawesome/free-solid-svg-icons';
 import './tabbar.css';
 
 export default function TabBar() {
   return (
     <nav className="nav">
-      <Link to="/" className="nav__link" tabIndex={0}>
-        <i className="fa-solid fa-house nav__icon"></i>
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? 'nav__link nav__link--active' : 'nav__link'
+        }
+        tabIndex={0}
+      >
+        <FontAwesomeIcon className="nav__icon" icon={faHouse} />
         <p className="nav__label">Home</p>
-      </Link>
-      <Link to="/watchlist" className="nav__link" tabIndex={0}>
-        <i className="fa-solid fa-heart nav__icon"></i>
+      </NavLink>
+      <NavLink
+        to="/watchlist"
+        className={({ isActive }) =>
+          isActive ? 'nav__link nav__link--active' : 'nav__link'
+        }
+        tabIndex={0}
+      >
+        <FontAwesomeIcon className="nav__icon" icon={faHeart} />
         <p className="nav__label">Watchlist</p>
-      </Link>
-      <Link to="/account" className="nav__link" tabIndex={0}>
-        <i className="fa-solid fa-user-gear nav__icon"></i>
+      </NavLink>
+      <NavLink
+        to="/account"
+        className={({ isActive }) =>
+          isActive ? 'nav__link nav__link--active' : 'nav__link'
+        }
+        tabIndex={0}
+      >
+        <FontAwesomeIcon className="nav__icon" icon={faUserGear} />
         <p className="nav__label">Account</p>
-      </Link>
+      </NavLink>
     </nav>
   );
 }
