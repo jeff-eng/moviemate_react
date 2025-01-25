fd469f345682ea62a0a4db722bed6ec36008bad1
jeff-eng <me@jeffeng.com>
2025-01-25 13:33:23 -0800

    Created Context provider, lifting state from Home component to parent App component. Helps keep track of search query and results on toggle between home, watchlist, and account.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,32 @@
-import './App.css';
+import { createContext, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/home/Home';
 import Watchlist from '../pages/watchlist/Watchlist';
 import Account from '../pages/account/Account';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './mainlayout/MainLayout';
+import './App.css';
 
-function App() {
+const AppContext = createContext();
+
+export default function App() {
+  const [results, setResults] = useState({});
+  const [searchQuery, setSearchQuery] = useState('');
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="watchlist" element={<Watchlist />} />
-          <Route path="account" element={<Account />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AppContext.Provider
+      value={{ results, setResults, searchQuery, setSearchQuery }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="watchlist" element={<Watchlist />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 }
 
-export default App;
+export { AppContext };
