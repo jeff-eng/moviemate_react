b67d60aede742065e363d722ab461d36272ee236
jeff-eng <me@jeffeng.com>
2025-03-11 13:10:26 -0700

    Added the TV Layout route components as well as specify the components for the detail pages to display in the Outlet.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/home/Home';
 import Watchlist from '../pages/watchlist/Watchlist';
 import Account from '../pages/account/Account';
-import MainLayout from './mainlayout/MainLayout';
+import MainLayout from './layouts/MainLayout';
 import './App.css';
 import './styles/searchresult.css';
+import MovieLayout from './layouts/MovieLayout';
+import MovieDetail from '../components/detail/MovieDetail';
+import TVLayout from '../components/layouts/TVLayout';
+import TVDetail from '../components/detail/TVDetail';
+import CastAndCrew from './castandcrew/CastAndCrew';
 
 const AppContext = createContext();
 
@@ -21,6 +26,14 @@ export default function App() {
         <Routes>
           <Route path="/" element={<MainLayout />}>
             <Route index element={<Home />} />
+            <Route path="movies/:id" element={<MovieLayout />}>
+              <Route index element={<MovieDetail />} />
+              <Route path="castandcrew" element={<CastAndCrew />} />
+            </Route>
+            <Route path="tv/:id" element={<TVLayout />}>
+              <Route index element={<TVDetail />} />
+              <Route path="castandcrew" element={<CastAndCrew />} />
+            </Route>
             <Route path="watchlist" element={<Watchlist />} />
             <Route path="account" element={<Account />} />
           </Route>
