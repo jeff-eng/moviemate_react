d3554e8ef9dffe431cfe3b2feb208f6295b81d74
jeff-eng <me@jeffeng.com>
2025-05-11 16:28:43 -0700

    Created initial pages for Account and Watchlist just to ensure it routing functions.

---
+++ src/pages/account/Account.jsx
@@ -0,0 +1,3 @@
+export default function Account() {
+  return <h1>This is the Account page</h1>;
+}
---
+++ src/pages/watchlist/Watchlist.jsx
@@ -0,0 +1,3 @@
+export default function Watchlist() {
+  return <h1>This is the watchlist page</h1>;
+}
